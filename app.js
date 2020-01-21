f5750c83e444d6fcdfd90f8f24480bfcedc6ac3e
eurioh <euri.elliot.oh@gmail.com>
2020-01-20 23:39:09 -0500

    Udemy code part I

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-// scope itmes array. It can store more than one item. New itme will be push at the end of array
-// const items = ["Grocery Shoping", "House Cleaning", "Dish Washing", "Laundry"];
-// const workItems=[];
-
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({
@@ -18,44 +14,90 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todoDB", {
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
     useUnifiedTopology: true
 });
 
-
-const todoItem = new mongoose.Schema({
+//create Schema (structure of objects)
+const itemSchema = new mongoose.Schema({
     content: {
         type: String,
-        require: [true, "Please check your data entry, no input detected"]
+        require: [true, "Wrong input"]
     }
 });
 
-//Create Schema with singular name of colleciton "List" -> it will turn into "lists"
-const TodoItem = mongoose.model("List", todoItem);
+//create model for collections 
+const Item = mongoose.model("Item", itemSchema);
+//Item: capital I, mongoose model is usually capitalized
+//item: singular version of collection name
+
+const itemZero = new Item({
+    content: "Welcome to your todo List!"
+});
+
+const itemOne = new Item({
+    content: "Hit the + button to add a new item."
+});
+
+const itemTwo = new Item({
+    content: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [itemZero, itemOne, itemTwo];
+
+
+
 
 app.get("/", function (req, res) {
+    //date.getDate() calling getDate function from date.js
+    let day = date.getDate();
 
-    TodoItem.find(function (err, items) {
 
-        res.render("List", {
-            kindofDay: date.getDay(),
-            newListItems: items.map(item => item.content)
-        });
+    Item.find({}, function (err, foundItems) {
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("succes!");
+                }
+            });
+            res.redirect("/");
+        } else {
+            // render ejs file name list
+            res.render("List", {
+                kindofDay: day,
+                newListItems: foundItems
+            });
+        }
     });
 });
 
 app.post("/", function (req, res) {
 
-    
-    const item = req.body.newItem;
 
-    const todoItem = new TodoItem({
-        content: item
+    const itemName = req.body.newItem; //from list.ejs name=newItem
+
+    const item = new Item({
+        content: itemName
+    })
+
+    item.save();
+    res.redirect("/");
+
+});
+
+app.post("/delete", function (req, res) {
+
+    // bring item.id value from list.ejs and store in checkedItemID
+    const checkedItemID = req.body.checkbox;
+
+    //following function (findByIdAndRemove) find the ID and delete the item related to ID. Warning: must have call back function
+    Item.findByIdAndRemove(checkedItemID, function(err) {
+        if (!err) {
+            res.redirect("/");
+        } 
     });
-    
-    todoItem.save();
-
-    res.redirect("/"); 
 });
 
 
